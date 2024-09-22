8a12b32b65a8ade18b679184254f82d69d7ec40f
Md Emdadullah <developeremdad@gmail.com>
2024-09-22 20:37:31 +0600

    make relation product with categories

--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -2,15 +2,30 @@ import { db } from "../../db.js";
 
 export const resolvers = {
   Query: {
+    // get all books
     books: () => db.books,
+    // get all users
     users: () => db.users,
+    // get all categories
     categories: () => db.categories,
+    // get a single category by category id
     category: (_parent: any, args: { categoryId: string }, _context: any) => {
       return db.categories.find((cate) => cate.id === args.categoryId);
     },
+    // Get all products
     products: () => db.products,
+    // get a single product by product id
     product: (_parent: any, args: { productId: string }, _context: any) => {
       return db.products.find((product) => product.id === args.productId);
+    },
+  },
+  Product: {
+    category: (parent: any, _args: any, _context: any) => {
+      // console.log(parent.categoryId);
+      const result = db.products.find(
+        (product) => product.categoryId === parent.categoryId
+      );
+      return result;
     },
   },
 };
